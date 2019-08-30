bf14d12c1a021edd1a50f304070fb744087651ca
Cristi Tarasi <tarasicristia@gmail.com>
2019-08-30 22:27:04 +0100

    I made the form and set up the the layout in broad strokes. Next I have to commit to a color palette

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,20 @@ import { hot } from 'react-hot-loader/root';
 
 export default hot(() => (
     <div className="App">
-        <h1 className="App_heading">Hello Task Maker</h1>
+        <div className="App header">
+            <h1 className="App_heading center">Task Maker</h1>
+        </div>
+
+        <div className="App_form">
+            <input type="text" className="App_input" placeholder="Type Task" />
+            <button className="App_form_button">Submit</button>
+        </div>
+
+        <ul className="App_list">
+            <li className="App_list_item">
+                <p className="App_copy">dfvmdfv</p>
+                <i class="fas fa-ellipsis-h"></i>
+            </li>
+        </ul>
     </div>
 ));

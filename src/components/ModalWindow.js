d94a942f58787dae61c1d61ea459fcf48e8683b9
Cristi Tarasi <tarasicristia@gmail.com>
2019-09-21 12:07:42 +0100

    added text editing buttons to the modal context

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -1,65 +1,63 @@
 import React, { useState, useRef } from 'react';
 import Modal from "@netojose/react-modal";
-
-const modalStyles = {
-    backgroundColor: '#c0cee2',
-    width: '256px',
-    height: '404px',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    boxShadow: '0 0 15px 5px rgba(0,0,0,0.3)',
-    borderRadius: '4px',
-    padding: '0',
-    margin: '0',
-    border: '0',
-};
+import { modalStyles } from './assets_mixed';
 
 export default function ({ isOpen, closeModal, task }) {
     const [noteText, setNoteText] = useState('Type your notes here.');
-    const [isEditable, setIsEditable] = useState(true);
+    const [isEditable, setIsEditable] = useState(false);
     const editElement = useRef();
 
+    // Function to turn the contenteditable mode to true - edit mode
+    // ***************************************************************
     function editNote () {
-        if (isEditable) editElement.current.contentEditable = true
-        else {
-            editElement.current.contentEditable = false;
-            setNoteText(editElement.current.innerText);
-        }
+        editElement.current.contentEditable = true;
+    }
+
+    // Function to turn the contenteditable mode to false - save mode
+    // ***************************************************************
+    function saveNote () {
+        editElement.current.contentEditable = false;
+        setNoteText(editElement.current.innerText);
     }
 
     return (
-        <Modal  
-            isOpen={isOpen} 
-            onRequestClose={closeModal}
-            modalStyles={modalStyles}
+        <Modal isOpen={!isOpen} onRequestClose={closeModal} modalStyles={modalStyles}
             overlayStyles={{backgroundColor: 'rgba(0,0,0,0.2)',}}>
-            
-            <div className="Modal_content">
-                <div className="Modal_header">
-                    <h1 className="Modal_title">
-                        {task.text.length >= 16 ? `${task.text.slice(0, 13)}...` : task.text}
-                    </h1>
-                    <span className="Modal_button_close">
-                        <i className="fas fa-times" onClick={closeModal}></i>
-                    </span>
+
+            <div className="Modal_body">
+                <div className="Modal_header_content">
+                    <div className="Modal_header">
+                        <h1 className="Modal_title">
+                            {task.text.length >= 16 ? `${task.text.slice(0, 13)}...` : task.text}
+                        </h1>
+                        <span className="Modal_button_close"><i className="fas fa-times" onClick={closeModal}></i></span>
+                    </div>
+                    <div className="Modal_button_group">
+                        <button className="Modal_button_edit_tools"><i class="fas fa-bold"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="fas fa-italic"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="fas fa-link"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="fas fa-image"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="far fa-image"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="fas fa-list-ul"></i></button>
+                        <button className="Modal_button_edit_tools"><i class="fas fa-list-ol"></i></button>
+                    </div>
+
+                    <div className="Modal_content" ref={editElement}>{noteText}</div>
                 </div>
-                <p className="Modal_content" ref={editElement}>{noteText}</p>
+
                 <div className="Modal_footer">
                     <i className="far fa-smile smile"></i>
                     <div className="Modal_button_group">
-                        <button 
-                            className="Modal_button edit" 
-                            onClick={() => editNote(setIsEditable(!isEditable))}
+                        <button
+                            className="Modal_button edit" onClick={editNote}
                         >Edit</button>
-                        <button 
+                        <button
                             className="Modal_button save"
-                            onClick={() => editNote(setIsEditable(!isEditable))}
+                            onClick={saveNote}
                         >Save</button>
                     </div>
-                </div>   
+                </div>
             </div>
         </Modal>
     );
-}    
+}
---
+++ src/components/assets_mixed.js
@@ -0,0 +1,18 @@
+// Modal popup window styles
+// ***************************************************************
+const modalStyles = {
+    backgroundColor: '#c0cee2',
+    width: '256px',
+    height: '404px',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    boxShadow: '0 0 15px 5px rgba(0,0,0,0.3)',
+    borderRadius: '4px',
+    padding: '0',
+    margin: '0',
+    border: '0',
+};
+
+export { modalStyles };
